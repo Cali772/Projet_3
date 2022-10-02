2bb0d95a2c03227b357d2dfd81f2c5e3e26146ca
Joris ZEFIRINI <joris.zefirini@vo2-group.com>
2022-10-02 10:43:25 -0400

    [FIX] Like button

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-setTimeout(() => {
-    document.querySelector('.preloader').style.display = 'none'
-},2240);
-
-document.querySelector('.like-button').addEventListener('click', (e) => {
-    alert('okk');
-    e.currentTarget.classlist.toggle('liked');
-
-});
+window.onload = function() {
+    setTimeout(() => {
+        document.querySelector('.preloader').style.display = 'none'
+    },2240);
+    
+    document.querySelectorAll(".like-button").forEach(elem => {
+        elem.addEventListener('click', (e) => {
+            e.currentTarget.classList.toggle('liked');
+        });
+    });
+}

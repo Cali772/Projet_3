f0d4c1eb973c4149646fc2af2095ae8346f71c5a
Joris ZEFIRINI <joris.zefirini@vo2-group.com>
2022-10-02 10:23:09 -0400

    [ADD] Rename and import script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 setTimeout(() => {
     document.querySelector('.preloader').style.display = 'none'
-
 },2240);
 
 document.querySelector('.like-button').addEventListener('click', (e) => {
+    alert('okk');
     e.currentTarget.classlist.toggle('liked');
 
 });
